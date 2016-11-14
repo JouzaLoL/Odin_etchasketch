0c1850612aefde52a801d392b2834983b58b6e18
Jouza <vacekj@outlook.com>
2016-11-14 19:42:34 +0100

    Add function to generate grid

---
+++ script/main.js
@@ -0,0 +1,13 @@
+
+
+/**
+ * 
+ */
+function generateGrid() {
+    var div = "<div></div>";
+    for (i = 0; i < 16; i++) {
+        for (i = 0; i < 16; i++) {
+            document.appendChild(div);
+        }
+    }
+}
